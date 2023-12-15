d556d38025c6ccf1b60f629c4c270e4d9fd2ea06
mounika.neerati <mounika.neerati93@gmail.com>
2023-12-15 14:36:36 +0530

    first commit

---
+++ variables_ in_js/index.js
@@ -0,0 +1,12 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z)
